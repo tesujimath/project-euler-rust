4879140d603813842d52f5817f0d0c83bb475c4e
Simon Guest <simon.guest@tesujimath.org>
2022-12-21 17:37:44 +1300

    Solution to problem 1 🎉

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    p1()
+}
+
+fn p1() {
+    let mut sum = 0;
+
+    for i in 1..1000 {
+        if i % 3 == 0 || i % 5 == 0 {
+            sum += i;
+        }
+    }
+    println!("p1: sum is {}", sum);
 }
