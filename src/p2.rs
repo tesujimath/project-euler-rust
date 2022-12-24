896baed36ce32562d315c0ff2e74434bae885da5
Simon Guest <simon.guest@tesujimath.org>
2022-12-24 15:42:23 +1300

    p2 with own is_even function

--- src/p2.rs
+++ src/p2.rs
@@ -2,23 +2,33 @@ use std::ops::Add;
 
 struct Fib<T>(T, T);
 
-impl<T> Fib<T>
+impl<T> Iterator for Fib<T>
 where
     T: Add<Output = T> + Copy,
 {
-    pub fn next(&mut self) -> T {
+    type Item = T;
+    fn next(&mut self) -> Option<T> {
         let result = self.0;
         let sum = self.0 + self.1;
         self.0 = self.1;
         self.1 = sum;
-        result
+        Some(result)
     }
 }
 
-pub fn run() {
-    let mut fib = Fib(1, 2);
+fn is_even(n: &i32) -> bool {
+    n % 2 == 0
+}
 
-    for _ in 1..10 {
-        println!("{}", fib.next());
-    }
+pub fn run() {
+    let fib = Fib(1, 2);
+    const MAX: i32 = 4_000_000;
+
+    let sum: i32 = fib
+        //.inspect(|n| println!("all {}", n))
+        .filter(is_even)
+        //.inspect(|n| println!("even {}", n))
+        .take_while(|n| n <= &MAX)
+        .sum();
+    println!("p2: sum is {}", sum);
 }
