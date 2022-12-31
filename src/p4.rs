5ba5c86385da7c7548b7006dfabaf435f9bd83bc
Simon Guest <simon.guest@tesujimath.org>
2022-12-31 17:46:58 +1300

    Opportunistic implementation for p4

--- src/p4.rs
+++ src/p4.rs
@@ -11,21 +11,57 @@ where
 
 #[test]
 fn test_is_palindrome() {
-    assert_eq!(is_palindrome(1), true);
-    assert_eq!(is_palindrome(12), false);
-    assert_eq!(is_palindrome(121), true);
-    assert_eq!(is_palindrome(1221), true);
+    assert!(is_palindrome(1));
+    assert!(!is_palindrome(12));
+    assert!(is_palindrome(121));
+    assert!(is_palindrome(1221));
 }
 
-pub fn run() {
+fn try_all() -> i32 {
+    let mut iterations = 0;
     let mut largest = 0;
     for i in 100..999 {
         for j in i..999 {
             let n = i * j;
+            iterations += 1;
             if is_palindrome(n) && n > largest {
                 largest = n;
             }
         }
     }
-    println!("p4: {}", largest);
+
+    println!("p4::try_all {} iterations", iterations);
+
+    largest
+}
+
+fn opportunistic() -> i32 {
+    let mut iterations = 0;
+    let mut largest = 0;
+    const UPPER: i32 = 999;
+    const LOWER: i32 = 100;
+    let mut i = UPPER;
+    while i * UPPER > largest && i >= LOWER {
+        let mut j = UPPER;
+
+        while i * j > largest && j >= LOWER {
+            let n = i * j;
+            iterations += 1;
+            if is_palindrome(n) && n > largest {
+                largest = n;
+            }
+
+            j -= 1;
+        }
+
+        i -= 1;
+    }
+
+    println!("p4::opportunistic {} iterations", iterations);
+
+    largest
+}
+
+pub fn run() {
+    println!("p4: {} {}", try_all(), opportunistic());
 }
